e75a5cf927e6cd2a50a883f5e0ec6a996dcd5974
Samuel Branisa <branisa.samuel@icloud.com>
2026-03-27 10:32:09 +0100

    Add greenmobility test code

---
+++ function/greenmobility_test.go
@@ -0,0 +1,12 @@
+// greenmobility_test.go
+
+package greenmobility
+
+import ( 
+    "testing"
+)
+
+func TestGreenMobility(t *testing.T) { 
+    // TODO: Add test logic here
+    t.Log("Testing Green Mobility...") 
+}
