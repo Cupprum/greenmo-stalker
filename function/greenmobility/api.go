7292025fe32472c9772797d8187600c838e4179b
Cupprum <branisa.samuel@icloud.com>
2026-04-08 12:21:20 +0200

    chore: cleanup the new code

--- function/greenmobility/api.go
+++ function/greenmobility/api.go
@@ -14,30 +14,6 @@ type Car struct {
 	Discount int
 }
 
-func getCar(endpoint string, id int) (Car, error) {
-	u, _ := url.Parse(fmt.Sprintf("%v/%v", endpoint, id))
-
-	resp, err := http.Get(u.String())
-	if err != nil {
-		return Car{}, fmt.Errorf("http get: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return Car{}, fmt.Errorf("status %d", resp.StatusCode)
-	}
-
-	var c struct {
-		Discount struct {
-			Percengate float64 `json:"discountPercentage"`
-		} `json:"discount"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&c); err != nil {
-		return Car{}, fmt.Errorf("decode: %w", err)
-	}
-	return Car{Discount: int(c.Discount.Percengate)}, nil
-}
-
 func Query(endpoint string, nw, se geo.Position, fuel int) ([]Car, error) {
 	center := geo.Position{Lat: (nw.Lat + se.Lat) / 2, Lon: (nw.Lon + se.Lon) / 2}
 	radius := geo.Distance(nw, se) / 2
@@ -60,7 +36,7 @@ func Query(endpoint string, nw, se geo.Position, fuel int) ([]Car, error) {
 		return nil, fmt.Errorf("status %d", resp.StatusCode)
 	}
 
-	var cars []struct {
+	var rawCars []struct {
 		ID  int `json:"id"`
 		SOC int `json:"stateOfCharge"`
 		Pos struct {
@@ -68,26 +44,43 @@ func Query(endpoint string, nw, se geo.Position, fuel int) ([]Car, error) {
 		} `json:"position"`
 		Benefit string `json:"benefit"`
 	}
-	if err := json.NewDecoder(resp.Body).Decode(&cars); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&rawCars); err != nil {
 		return nil, fmt.Errorf("decode: %w", err)
 	}
 
 	var res []Car
-	for _, c := range cars {
+	for _, c := range rawCars {
 		pos := geo.Position{Lat: c.Pos.Coords[1], Lon: c.Pos.Coords[0]}
+
 		// Greenmo thinks in circles, we think in squares
 		inBox := nw.Lon < pos.Lon && pos.Lon < se.Lon && se.Lat < pos.Lat && pos.Lat < nw.Lat
 		if c.SOC <= fuel && inBox {
 			discount := 0
 			if c.Benefit == "DISCOUNTED" {
-				details, err := getCar(endpoint, c.ID)
-				if err != nil {
-					return nil, fmt.Errorf("failed to get car: %w", err)
-				}
-				discount = details.Discount
+				// NOTE: on failure we ignore discount
+				discount, _ = fetchDiscount(endpoint, c.ID)
 			}
 			res = append(res, Car{Pos: pos, Charge: c.SOC, Discount: discount})
 		}
 	}
 	return res, nil
+}
+
+func fetchDiscount(endpoint string, id int) (int, error) {
+	u := fmt.Sprintf("%s/%d", endpoint, id)
+	resp, err := http.Get(u)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var data struct {
+		Discount struct {
+			Percentage float64 `json:"discountPercentage"`
+		} `json:"discount"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return 0, err
+	}
+	return int(data.Discount.Percentage), nil
 }
